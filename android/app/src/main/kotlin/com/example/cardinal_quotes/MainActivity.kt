0076a9b4d44c27114b7964dc00c584d28fbe7ce0
theodan11 <theophiljadango@gmail.com>
2025-09-18 15:18:51 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cardinal_quotes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cardinal_quotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/theme/custom_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color primaryBgColor = Color(0xFFC13B20);
+  static final Color primaryColor = Color(0xFF591A0E);
+
+  static final Color cardWhiteColor = Color(0xFFFEF0D6);
+
+  static final Color btnColor = Color(0xFF52180D);
+  static final Color btnDisColor = Color(0xAB52190D);
+}
